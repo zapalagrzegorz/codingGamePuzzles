f5b32470e184e0eb988a4713dd49715e3127c142
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-05-29 00:37:20 +0200

    refactoring

--- brackets.ts
+++ brackets.ts
@@ -1,8 +1,7 @@
 // https://www.codingame.com/ide/puzzle/brackets-extreme-edition
 
-
 // const expression = readline();
-const expression = '{[{iHTSc}]}p(R)m(){q({})';
+const expression = '{([]){}()}';
 // const expression = '][';
 // const expression = ']';
 
@@ -19,61 +18,70 @@ function result() {
 
   let indexOpenedBrackets = 0;
 
+  let result = true;
+
   function add(char: string) {
     brackets[char] += ++indexOpenedBrackets;
   }
+
   function remove(char: string) {
     brackets[char] -= indexOpenedBrackets--;
   }
 
-
-  for (const element of expression.split('')) {
-    if(indexOpenedBrackets === -1){
-      console.log('false');
-      return;
-    }
-
-    switch (element) {
-      case '{':
-        add('{');
-        break;
-
-      case '}':
-        remove('{');
-        break;
-
-      case '(':
-        add('(');
-        break;
-
-      case ')':
-        remove('(');
-        break;
-
-      case '[':
-        add('[');
-        break;
-
-      case ']':
-        remove('[');
-        break;
-    }
-  }
-
   // corner case of single 'closing bracket'
-  if(indexOpenedBrackets === -1){
-    console.log('false');
-    return;
-  }
-
-  for (const bracket in brackets) {
-    if (brackets[bracket] !== 0) {
-      console.log('false');
-      return;
+  function catchClosingBrace() {
+    if (indexOpenedBrackets === -1) {
+      result = false;
     }
   }
 
-  console.log('true');
+  function calculateBraces() {
+    for (const element of expression.split('')) {
+      catchClosingBrace();
+
+      switch (element) {
+        case '{':
+          add('{');
+          break;
+
+        case '}':
+          remove('{');
+          break;
+
+        case '(':
+          add('(');
+          break;
+
+        case ')':
+          remove('(');
+          break;
+
+        case '[':
+          add('[');
+          break;
+
+        case ']':
+          remove('[');
+          break;
+      }
+    }
+  }
+
+  function printResult() {
+    for (const bracket of Object.keys(brackets)) {
+      if (brackets[bracket] !== 0) {
+        result = false;
+      }
+    }
+    console.log(result);
+  }
+
+  calculateBraces();
+
+  catchClosingBrace();
+
+  printResult();
+
 }
 
 result();
